184d41f41a22962cc3d2cbb82bbe11f1b73f830e
shaikhwajid45 <108930211+shaikhwajid45@users.noreply.github.com>
2022-07-08 15:55:10 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multitreading
+{
+    class Person
+    {
+
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+        public Person()
+        {
+            Fname = "wajid";
+            Lname = "shaikh";
+        }
+        public Person(string f, string l )
+        {
+            Fname = f;
+            Lname = l;
+        }
+        ~Person()    
+        {
+            Console.WriteLine("Hii from Gc");
+
+        }
+    }
+}
